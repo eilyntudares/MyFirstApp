c3adbac461d81b298537e86603b36a855c23a8a4
Eilyn Tudares <etuda001@fiu.edu>
2025-05-10 17:06:53 -0400

    Initial commit

--- MyFirstApp/ViewController.swift
+++ MyFirstApp/ViewController.swift
@@ -14,6 +14,40 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+    func changeColor() -> UIColor {
+        let red = CGFloat.random(in: 0...1)
+        let green = CGFloat.random(in: 0...1)
+        let blue = CGFloat.random(in: 0...1)
+        return UIColor(red: red, green: green, blue: blue, alpha: 0.5)
+    }
 
+    @IBAction func changeBackgroundColor(_ sender: UIButton) {
+        let randomColor = changeColor()
+        view.backgroundColor = randomColor
+        
+        nameLabel.textColor = .white
+        universityLabel.textColor = .white
+        jobLabel.textColor = .white
+        
+        let imageNames = ["cat1", "cat2", "cat3"]
+        let randomIndex = Int.random(in: 0..<imageNames.count)
+        let randomImage = UIImage(named: imageNames[randomIndex])
+        UIView.transition(with: dynamicImageView,
+        duration: 0.5,
+        options: .transitionCrossDissolve,
+        animations: {
+        self.dynamicImageView.image = randomImage},
+        completion: nil)
+
+
+    }
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var universityLabel: UILabel!
+    @IBOutlet weak var jobLabel: UILabel!
+
+    @IBOutlet weak var dynamicImageView: UIImageView!
+    
+    
 }
 
